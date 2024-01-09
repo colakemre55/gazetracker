4153111005adc861283d45d24135a46da11d4edc
Emre Çolak <43623772+emrecolak55@users.noreply.github.com>
2024-01-09 20:42:15 +0300

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,125 @@
+﻿namespace WindowsFormsApp_EMGUCVBase
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.exitButton = new System.Windows.Forms.Button();
+            this.axWindowsMediaPlayer1 = new AxWMPLib.AxWindowsMediaPlayer();
+            this.PlayMedia = new System.Windows.Forms.Button();
+            this.UploadVideo = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.axWindowsMediaPlayer1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(535, 310);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // exitButton
+            // 
+            this.exitButton.Location = new System.Drawing.Point(874, 654);
+            this.exitButton.Name = "exitButton";
+            this.exitButton.Size = new System.Drawing.Size(156, 44);
+            this.exitButton.TabIndex = 2;
+            this.exitButton.Text = "Exit";
+            this.exitButton.UseVisualStyleBackColor = true;
+            this.exitButton.Click += new System.EventHandler(this.exitButton_Click);
+            // 
+            // axWindowsMediaPlayer1
+            // 
+            this.axWindowsMediaPlayer1.Enabled = true;
+            this.axWindowsMediaPlayer1.Location = new System.Drawing.Point(12, 12);
+            this.axWindowsMediaPlayer1.Name = "axWindowsMediaPlayer1";
+            this.axWindowsMediaPlayer1.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("axWindowsMediaPlayer1.OcxState")));
+            this.axWindowsMediaPlayer1.Size = new System.Drawing.Size(701, 502);
+            this.axWindowsMediaPlayer1.TabIndex = 3;
+            // 
+            // PlayMedia
+            // 
+            this.PlayMedia.Location = new System.Drawing.Point(316, 654);
+            this.PlayMedia.Name = "PlayMedia";
+            this.PlayMedia.Size = new System.Drawing.Size(156, 44);
+            this.PlayMedia.TabIndex = 4;
+            this.PlayMedia.Text = "PlayVideo";
+            this.PlayMedia.UseVisualStyleBackColor = true;
+            this.PlayMedia.Click += new System.EventHandler(this.PlayMedia_Click);
+            // 
+            // UploadVideo
+            // 
+            this.UploadVideo.Location = new System.Drawing.Point(591, 654);
+            this.UploadVideo.Name = "UploadVideo";
+            this.UploadVideo.Size = new System.Drawing.Size(156, 44);
+            this.UploadVideo.TabIndex = 5;
+            this.UploadVideo.Text = "UploadVideo";
+            this.UploadVideo.UseVisualStyleBackColor = true;
+            this.UploadVideo.Click += new System.EventHandler(this.UploadVideo_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick_1);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1262, 913);
+            this.Controls.Add(this.UploadVideo);
+            this.Controls.Add(this.PlayMedia);
+            this.Controls.Add(this.axWindowsMediaPlayer1);
+            this.Controls.Add(this.exitButton);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.axWindowsMediaPlayer1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button exitButton;
+        private AxWMPLib.AxWindowsMediaPlayer axWindowsMediaPlayer1;
+        private System.Windows.Forms.Button PlayMedia;
+        private System.Windows.Forms.Button UploadVideo;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,181 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Windows.Forms;
+using Emgu.CV;
+using MediaToolkit;
+using MediaToolkit.Model;
+using Microsoft.DirectX.AudioVideoPlayback; // These 2 dll were added by references 
+using Microsoft.DirectX; // 
+using System.Drawing;
+
+namespace WindowsFormsApp_EMGUCVBase
+{
+    public partial class Form1 : Form
+    {
+        readonly VideoCapture cameraCapture; // readonly?
+        readonly VideoCapture videoFileCapture;
+
+        private string selectedFilePath;
+
+        //string fileToSaveFrames = @"C:\Users\colak\Downloads\popcorn.mp4";
+
+        // video directx part
+        private Video video;
+        private string[] videoPaths;
+        private string folderPath = @"C:\Users\colak\Downloads\popcorn.mp4";
+        private Size formSize;
+        private Size pnlSize;
+
+
+
+        public Form1()
+        {
+            InitializeComponent();
+            this.FormClosing += Form1_FormClosing;
+
+            //directx part
+            //VolumeTrackBar.Value = 4;
+        }
+     
+        private void Form1_Load(object sender, System.EventArgs e)
+        {
+            formSize = new Size(this.Width , this.Height) ;
+            pnlSize = new Size(pnlVideo.Width, pnlVideo.Height) ;
+
+            videoPaths = Directory.GetFiles(folderPath, "*.wmv");
+
+            if( videoPaths != null)
+            {
+
+            }
+        }
+
+        private void CaptureFramesFromCamera()
+        {
+            int i = 0; // for file names
+            using (var cameraVideo = new VideoCapture(0))
+            using (var img = new Mat())
+            {
+                while (cameraVideo.Grab())
+                {
+                    cameraVideo.Retrieve(img);
+                    var filename = Path.Combine("C:\\Users\\colak\\source\\repos\\WindowsFormsApp EMGUCVBase\\Frames\\", $"Camera_{i}.png");
+                    CvInvoke.Imwrite(filename, img);
+                    i++;
+                }
+            }
+
+
+        }
+
+        private void CaptureFramesFromVideoFile(string filePath)
+        {
+            int i = 0; // for file names
+            using (var displayedVideo = new VideoCapture(filePath))
+            using (var img = new Mat())
+            {
+                while (displayedVideo.Grab())
+                {
+                    displayedVideo.Retrieve(img);
+                    var filename = Path.Combine("C:\\Users\\colak\\source\\repos\\WindowsFormsApp EMGUCVBase\\Frames\\", $"Video_{i}.png");
+                    CvInvoke.Imwrite(filename, img);
+                    i++;
+                }
+            }
+
+
+        }
+
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (cameraCapture != null && cameraCapture.IsOpened)
+                cameraCapture.Dispose();
+
+            if (videoFileCapture != null && videoFileCapture.IsOpened)
+                videoFileCapture.Dispose();
+        }
+
+
+        private void exitButton_Click(object sender, System.EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void PlayMedia_Click(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrEmpty(selectedFilePath))
+            {
+                axWindowsMediaPlayer1.URL = selectedFilePath;
+                axWindowsMediaPlayer1.Ctlcontrols.play();
+
+                System.Threading.Thread cameraThread = new System.Threading.Thread(() => CaptureFramesFromCamera());
+                cameraThread.Start();
+                System.Threading.Thread dispalyVideoThread = new System.Threading.Thread(() => CaptureFramesFromVideoFile(selectedFilePath));
+                dispalyVideoThread.Start();
+
+            }
+            else
+            {
+                MessageBox.Show("Please select a media file first.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void timer1_Tick_1(object sender, EventArgs e)
+        {
+            PlayingPosition = CalculateTime(video.CurrentPosition);
+            txtStatus.Text = PlayingPosition + "/" + Duration;
+            if (video.CurrentPosition >= video.Duration)
+            {
+                timer1.Stop();
+                Duration = CalculateTime(vdo.Duration);
+                PlayingPosition = "0:00:00";
+                txtStatus.Text = PlayingPosition + "/" + Duration;
+                video.Stop();
+                btnPlay.BackgroundImage = Play_Video.Properties.Resources.btnplay;
+                vdoTrackBar.Value = 0;
+            }
+            else
+                vdoTrackBar.Value += 1;
+        }
+        public string CalculateTime(double Time)
+        {
+            string mm, ss, CalculatedTime;
+            int h, m, s, T;
+            Time = Math.Round(Time);
+            T = Convert.ToInt32(Time);
+            h = (T / 3600);
+            T = T % 3600;
+            m = (T / 60);
+            s = T % 60;
+            if (m < 10)
+                mm = string.Format("0{0}", m);
+            else
+                mm = m.ToString();
+            if (s < 10)
+                ss = string.Format("0{0}", s);
+            else
+                ss = s.ToString();
+            CalculatedTime = string.Format("{0}:{1}:{2}", h, mm, ss);
+            return CalculatedTime;
+        }
+
+        private void UploadVideo_Click(object sender, EventArgs e)
+        {
+            using (OpenFileDialog ofd = new OpenFileDialog() { Multiselect = false, ValidateNames = true, Filter = "MP4|*.mp4|WMV|*.wmv|WAV|*.wav|MP3|*.mp3|MKV|*.mkv" })
+            {
+                if (ofd.ShowDialog() == DialogResult.OK)
+                {
+                    FileInfo chosenFile = new FileInfo(ofd.FileName);
+                    selectedFilePath = chosenFile.FullName;
+                }
+            }
+        }
+    }
+}
